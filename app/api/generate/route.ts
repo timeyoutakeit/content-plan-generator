eef2a46420ec3a84f5df45b425e005696b35e35f
Amy Burns <amy.burns87@gmail.com>
2025-10-08 20:14:20 +0100

    removing comment

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,18 +1,5 @@
 import { GoogleGenAI } from "@google/genai";
 
-// // The client gets the API key from the environment variable `GEMINI_API_KEY`.
-// const ai = new GoogleGenAI({});
-
-// async function main() {
-//   const response = await ai.models.generateContent({
-//     model: "gemini-2.5-flash",
-//     contents: "Explain how AI works in a few words",
-//   });
-//   console.log(response.text);
-// }
-
-// main();
-
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 export async function POST(req: Request) {
